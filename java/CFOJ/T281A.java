e06a7aec5ebeb7c298f5c32a68e3db8237f64640
apersonlieksc <1194261806@qq.com>
2018-01-04 11:58:49 +0800

    Word_Capitalization

---
+++ java/CFOJ/T281A.java
@@ -0,0 +1,15 @@
+package sosoalgorithm.java.CFOJ;
+
+import java.util.Scanner;
+
+public class T281A {
+	public static void main(String[] args) {
+		Scanner sin = new Scanner(System.in);
+		while(sin.hasNext()) {
+			String s = sin.next();
+			s = s.substring(0, 1).toUpperCase()+s.substring(1);
+			System.out.println(s);
+		}
+		sin.close();
+	}
+}
