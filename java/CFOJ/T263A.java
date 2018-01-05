c8f3a29f933b3e4432e016d063a4c0cd4e8f72c8
apersonlieksc <1194261806@qq.com>
2018-01-05 19:40:25 +0800

    Beautiful_Matrix

---
+++ java/CFOJ/T263A.java
@@ -0,0 +1,25 @@
+package sosoalgorithm.java.CFOJ;
+
+import java.util.Scanner;
+
+public class T263A {
+	public static void main(String[] args) {
+		Scanner sin = new Scanner(System.in);
+		while(sin.hasNext()) {
+			int k = 0;
+			int[][] a = new int[5][5];
+			for(int i = 0;i<5;i++) {
+				for(int j = 0;j<5;j++) {
+					a[i][j] = sin.nextInt();
+					if(a[i][j] == 1) {
+						k = Math.abs(2-i)+Math.abs(2-j);
+						//System.out.println(i);
+						//System.out.println(j);
+					}
+				}
+			}
+			System.out.println(k);
+		}
+		sin.close();
+	}
+}
