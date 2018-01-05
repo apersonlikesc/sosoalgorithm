87f066c001cb22d373c84d0bccbb9f2ce96ee4b9
apersonlieksc <1194261806@qq.com>
2018-01-05 19:40:50 +0800

    HQ9+

---
+++ java/CFOJ/T133A.java
@@ -0,0 +1,25 @@
+package sosoalgorithm.java.CFOJ;
+
+import java.util.Scanner;
+
+public class T133A {
+	public static void main(String[] args) {
+		Scanner sin = new Scanner(System.in);
+		while(sin.hasNext()) {
+			String s = sin.next();
+			boolean status = false;
+			for(int i = 0;i<s.length();i++) {
+				String s1 = s.substring(i, i+1);
+				if(s1.matches("[HQ9]")) {
+					status = true;
+				}
+			}
+			if(status) {
+				System.out.println("YES");
+			}else {
+				System.out.println("NO");
+			}
+		}
+		sin.close();
+	}
+}
