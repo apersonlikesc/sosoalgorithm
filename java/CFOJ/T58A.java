4fa9a5cd3f4b83e955e10de14b60b6680c5455a2
apersonlieksc <1194261806@qq.com>
2018-01-06 11:11:35 +0800

    Chat_room

---
+++ java/CFOJ/T58A.java
@@ -0,0 +1,18 @@
+package sosoalgorithm.java.CFOJ;
+
+import java.util.Scanner;
+
+public class T58A {
+	public static void main(String[] args) {
+		Scanner sin = new Scanner(System.in);
+		while(sin.hasNext()) {
+			String s = sin.next();
+			if(s.matches("(.*?)(h)(.*?)(e)(.*?)(l)(.*?)(l)(.*?)(o)(.*?)")) {
+				System.out.println("YES");
+			}else {
+				System.out.println("NO");
+			}
+		}
+		sin.close();
+	}
+}
