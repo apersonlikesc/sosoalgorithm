88c746375e4d7e616ec9284eae746f97ebca73d0
apersonlieksc <1194261806@qq.com>
2018-01-06 17:38:34 +0800

    solider_and_banana

---
+++ java/CFOJ/T546A.java
@@ -0,0 +1,24 @@
+package sosoalgorithm.java.CFOJ;
+
+import java.util.Scanner;
+
+public class T546A {
+	public static void main(String[] args) {
+		Scanner sin = new Scanner(System.in);
+		while(sin.hasNext()) {
+			int k = sin.nextInt();
+			int n = sin.nextInt();
+			int w = sin.nextInt();
+			int sum = 0;
+			for(int i = 0;i<w;i++) {
+				sum = sum + (i+1)*k;
+			}
+			if(sum>n) {
+				System.out.println(sum - n);
+			}else {
+				System.out.println(0);
+			}
+		}
+		sin.close();
+	}
+}
