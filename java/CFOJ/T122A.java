dada20cf2e22d0e00e56ab4a4f2e8f675c17a16a
apersonlieksc <1194261806@qq.com>
2018-01-06 15:25:26 +0800

    Lucky_Division

---
+++ java/CFOJ/T122A.java
@@ -0,0 +1,31 @@
+package sosoalgorithm.java.CFOJ;
+
+import java.util.Scanner;
+
+public class T122A {
+	public static void main(String[] args) {
+		Scanner sin = new Scanner(System.in);
+		while(sin.hasNext()) {
+			String s = sin.next();
+			boolean status = false;
+			int[] a = {4,44,444,7,77,777,47,74,474,747,477,744};
+			if(s.matches("((4|7)*?)")){
+				System.out.println("YES");
+			}else {
+				for(int i = 0;i<a.length;i++) {
+					if(Integer.valueOf(s)%a[i] == 0) {
+						System.out.println("YES");
+						status = true;
+						break;
+					}
+				}
+				if(status == false) {
+					System.out.println("NO");
+				}
+				
+				
+			}
+		}
+		sin.close();
+	}
+}
