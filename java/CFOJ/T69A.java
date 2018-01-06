6d6591c2864205fb7f35827f62f1b8bae8f97dd1
apersonlieksc <1194261806@qq.com>
2018-01-06 18:05:06 +0800

    Young_Physicist

--- java/CFOJ/T69A.java
+++ java/CFOJ/T69A.java
@@ -6,15 +6,18 @@ public class T69A {
 	public static void main(String[] args) {
 		Scanner sin = new Scanner(System.in);
 		while(sin.hasNext()) {
-			int sum = 0;
+			int suma=0,sumb=0,sumc = 0;
 			int number= sin.nextInt();
 			for(int i = 0;i<number;i++) {
 				int a = sin.nextInt();
 				int b = sin.nextInt();
 				int c = sin.nextInt();
-				sum = a+ b + c;
+				suma = suma + a;
+				sumb = sumb + b;
+				sumc = sumc + c;
+
 			}
-			if(sum == 0) {
+			if(suma == 0&&sumb == 0&&sumc == 0) {
 				System.out.println("YES");
 			}else {
 				System.out.println("NO");
