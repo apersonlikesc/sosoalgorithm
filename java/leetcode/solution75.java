350fe809055df773065fdbd475fc2a51fdc750c7
apersonlikesc <1194261806@qq.com>
2019-04-15 11:38:52 +0800

    2019年 04月 12日 星期五 14:44:51 CST

--- java/leetcode/solution75.java
+++ java/leetcode/solution75.java
@@ -3,7 +3,22 @@ public class solution75 {
         int zero = -1;
         int one = 0;
         int two = nums.length;
+        while(one < two)
+        {
+            if(nums[one] == 0)
+            {
+                swap(nums,++zero,one++);
+            }else if(nums[one] == 2){
+                swap(nums,--two,one++);
+            }else{
+                one++;
+            }
+        }
+    }
 
-
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
